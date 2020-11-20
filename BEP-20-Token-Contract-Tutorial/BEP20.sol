7a263078bae3de682901919164ab84dd796224cd
coderrick <derrick.duncan@temple.edu>
2020-11-19 20:39:04 -0500

    Revised contract

--- BEP-20-Token-Contract-Tutorial/BEP20.sol
+++ BEP-20-Token-Contract-Tutorial/BEP20.sol
@@ -1,19 +1,22 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 //Below are the 6 functions needed to implement the erc20 standard
-interface ERC20 {
+interface BEP20 {
   function totalSupply() external view returns (uint256);
   function balanceOf(address account) external view returns (uint256);
   function transfer(address recipient, uint256 amount) external returns (bool);
   function allowance(address owner, address spender) external view returns (uint256);
   function approve(address spender, uint256 value) external returns (bool);
   function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+  function name(string name) external returns (string);
+  function symbol(string symbol) external returns (string);
+  function decimals(uint8 digits) external returns (uint8);
   event Transfer(address from, address to, uint256 value);
   event Approval(address owner, address spender, uint256 value);
 }
 
 //The BEP20 standard shares the same 6 functions from ERC20
-contract BEP20 is ERC20 {
+contract AppleToken is BEP20 {
     
   mapping (address => uint) balances;
   
@@ -41,20 +44,18 @@ contract BEP20 is ERC20 {
     //TODO
   }
   
-  //BEP20 standard exclusive functions are the following
-  
   //Returns the name of the token - e.g. "MyToken"
-  function name() public view returns (string){
+  function name() external returns (string  memory){
     //TODO
   }
   
   //Returns the symbol of the token. E.g. “HIX”.
-  function symbol() public view returns (string){
+  function symbol() external returns (string  memory){
     //TODO
   }
   
   //Returns the number of decimals the token uses
-  function decimals() public view returns (uint8){
+  function decimals() external returns (uint8){
     //TODO
   }
 }
