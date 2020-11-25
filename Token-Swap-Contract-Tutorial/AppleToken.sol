99a54e8709f746af9162bef2f8faf3cbb9c01d87
coderrick <derrick.duncan@temple.edu>
2020-11-24 23:57:13 -0500

    Implemented balanceOf in Apple Token

--- Token-Swap-Contract-Tutorial/AppleToken.sol
+++ Token-Swap-Contract-Tutorial/AppleToken.sol
@@ -18,14 +18,14 @@ contract AppleToken is BEP20 {
     
   mapping (address => uint) balances;
   
-  uint256 totalSupply = 500000;
+  uint256 cap = 500000;
   
   function totalSupply() external view returns (uint256){
-    return totalSupply;
+    return cap;
   }
   
-  function balanceOf(address account) external view returns (uint256){
-    //TODO
+  function balanceOf(address account_address) external view returns (uint256){
+    return balances[account_address];
   }
   
   function transfer(address recipient, uint256 amount) external returns (bool){
